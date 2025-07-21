08aead2887993dda2109fdc58a5d042c76249bca
freakymind12 <riankurniawan132@gmail.com>
2025-07-21 10:45:33 +0700

    fix: enable time writting to polling plc

--- src/modbus/init.js
+++ src/modbus/init.js
@@ -13,20 +13,17 @@ async function initModbusService(callback) {
 
 
   // Write jam, menit, detik ke plc polling dengan interval 1 detik
-  // setInterval(async () => {
-  // await writeRegister(
-  // modbusClient,
-  // 10,
-  // [
-  //   dayjs().hour(),
-  //   dayjs().minute(),
-  //   dayjs().second(),
-  //   dayjs().date(),
-  //   dayjs().month() + 1,
-  //   dayjs().year()
-  // ]
-  //   );
-  // }, 1000);
+  setInterval(async () => {
+  await writeRegister(
+  modbusClient,
+  10,
+  [
+    dayjs().hour(),
+    dayjs().minute(),
+    dayjs().second(),
+  ]
+    );
+  }, 1000);
 
   // Return object atau fungsi yang kamu butuh
   return true;
